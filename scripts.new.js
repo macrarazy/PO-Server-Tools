4aac73dae5d3ebe9c8acb37438763070f4863b2d
TheUnknownOne <TheUnknownOne25@Gmail.com>
2012-12-09 00:52:26 +0100

    test2

--- scripts.new.js
+++ scripts.new.js
@@ -145,7 +145,7 @@ BRANCH = "alpha";
  * @type {Array}
  */
 Modules = [
-    "modules/jsext.js", "modules/utilities.js", "modules/cache.js", "modules/datahash.js"//,
+    "modules/jsext.js"//, "modules/utilities.js", "modules/cache.js", "modules/datahash.js",
     //"modules/jsession.js", "modules/users.js", "modules/channels.js", "modules/templates.js",
     //"modules/mafia.js"
 ];
